56f7d04fe84fc9ee8a673b3bec98b5ebe8e1b554
cowsbunga <86610385+cowsbunga@users.noreply.github.com>
2024-03-20 16:33:41 +0000

    Add files via upload

---
+++ pub game simulator v3.py
@@ -0,0 +1,81 @@
+import numpy as np
+import statistics
+import random
+import matplotlib.pyplot as plt
+
+
+#simulates a game
+def game(glasses, glasses_no):
+    
+    new_amounts = [0.0] * glasses_no
+    choice = 0
+
+
+    for i in range(1000):
+        #Ali's turn
+
+        for j in range(glasses_no):
+            new_amounts[j] = random.random() 
+
+        total = 2*sum(new_amounts)
+        for j in range(glasses_no):
+            new_amounts[j] = new_amounts[j] / total
+            glasses[j] = glasses[j] + new_amounts[j]
+
+
+
+
+        #test if glass overflows 
+        for j in glasses:
+            if j > 1:
+                return("Ali")
+                
+
+        #Beth's turn
+        j_max = 0
+        for j in range(glasses_no):
+            if glasses[j] >= j_max:
+                j_max = glasses[j]
+                index = j
+        
+        glasses[index] = 0.0
+        if glasses[(index+1) % glasses_no] > glasses[(index-1) % glasses_no]:
+            glasses[(index+1) % glasses_no] = 0.0
+        else:
+            glasses[(index-1) % glasses_no] = 0.0
+
+
+    return("Beth")
+
+
+
+
+#simulates many games and creates a histogram of the results
+iterations = 1000
+Ali_win_rate = [0.5] * 5
+
+for n in range(3, 8):
+    results = [0.0] * iterations
+    for i in range(iterations):
+        glasses = [0.0] * n
+        outcome = game(glasses, n)
+        if outcome == "Ali":
+            results [i] = 1.0
+    Ali_win_rate[n-3] = statistics.mean(results)
+    print(n, ": ", Ali_win_rate[n-3])
+
+
+'''
+xpoints = range(3, 8)
+
+plt.plot(xpoints, Ali_win_rate)
+plt.xlabel("Number of glasses")
+plt.ylabel("Ali's winrate")
+plt.ylim(-0.2, 1.2)
+
+
+plt.show()
+    
+print(Ali_win_rate[0])
+
+'''
